58ae5359fd37899ad55feb095d9ae7b524be158c
Robert Abreu <robertabreu@pursuit.org>
2019-01-20 20:15:35 -0500

    episode cards now reveal hidden info; image resized

--- js/app.js
+++ js/app.js
@@ -8,27 +8,47 @@ const episodes = document.querySelector('.js-episodes');
 
 
 const episodeCard = (name, host, guest, description, img, link) => {
-
-    return innerHTML = `
-    <div class="col s12 m7">
-        <h2 class="header">${name}</h2>
-        <div class="card horizontal">
-            <div class="card-image">
-                <img src="${img}" style='height: auto; width: 200px';>
-                <span class="card-title black-text flow-text" style='font-size: 20px;'>${guest}</span> 
-            </div>
-            <div class="card-stacked">
-                <div class="card-content">
-                    <p>${description}</p>
+    /*
+        return innerHTML = `
+        <div class="col s12 m7">
+            <h2 class="header">${name}</h2>
+            <div class="card horizontal">
+                <div class="card-image">
+                    <img src="${img}" style='height: auto; width: 200px';>
+                    <span class="card-title black-text flow-text" style='font-size: 20px;'>${guest}</span> 
                 </div>
-                <div class="card-action">
-                    <a href="${link}">LISTEN TO EPISODE</a>
+                <div class="card-stacked">
+                    <div class="card-content">
+                        <p>${description}</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="${link}">LISTEN TO EPISODE</a>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    `
+        `
+    */
 
+    return innerHTML = `
+    <div class="card horizontal">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="${img}" style="height: auto; width: 200px;">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4"><span class='h3'>${name} <span class='grey-text'>// ${guest}</span></span><i class="material-icons right">more_vert</i></span>
+      <p>${description}</p>
+        <div class="m-0 card-action">
+            <a href="${link}">LISTEN TO EPISODE</a>
+        </div>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4"><span class='h3'>${name} <span class='grey-text'>// ${guest}</span></span><i class="material-icons right">close</i></span>
+      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+    </div>
+  </div>
+        
+    `
 };
 
 
