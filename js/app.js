0333d86047641b9a9eba6baf690dd09f3adc6131
Robert Abreu <robertabreu@pursuit.org>
2019-01-29 21:00:33 -0500

    added overflow to text and set max height for episodes

--- js/app.js
+++ js/app.js
@@ -71,13 +71,13 @@ const episodeCard = (episodes) => {
         // e.name, e.host, e.host_img, e.guest, e.description, e.image, e.link
         // flow-text
         innerHTML += `
-        <div class="col s12 row card horizontal" data-index=${i}>
+        <div class="col s12 row card horizontal" data-index=${i} style="height: 200px;">
             <div class="col s5 card-image waves-effect waves-block waves-light">
                 <img class="activator" src="${e.image}" style="height: auto; width: 200px;">
             </div>
-            <div class="col s7 card-content row">
+            <div class="col s7 card-content  row" >
                 <span class="card-title activator grey-text text-darken-4"><span class='h3'>${e.name} <span class='grey-text'>// ${e.guest}</span></span><i class="material-icons right">more_vert</i></span>
-                <p class=''>${e.description}</p>
+                <p class='' style="height: 80px; overflow: scroll;">${e.description}</p>
                 <div class="m-0 card-action">
                     ${isLinkAvailable(e.link)}
                 </div>
@@ -170,7 +170,7 @@ let state = {
             name: 'Pilot',
             host: ['Daniel'],
             guest: 'Victoria Buchanan',
-            description: 'Guest description goes here',
+            description: 'Pending description.',
             image: 'assets/images/victoria_buchanan.png',
             guest_link: 'https://github.com/VictoriaBuchanan27',
             link: 'https://soundcloud.com/codebreak-fivetwo/codebreak-pilot-victoria/s-FjgMP',
